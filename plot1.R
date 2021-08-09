5bf73c7da698879bb9d5cb61abb8b6f969b28c0a
Kevin Roche <kevinroche222@gmail.com>
2021-08-09 10:59:53 -0400

    Initial commit

---
+++ plot1.R
@@ -0,0 +1,29 @@
+## Load packages
+library(tidyverse)
+library(ggplot2)
+
+## Load Data
+NEI <- readRDS("exdata_data_NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS("exdata_data_NEI_data/Source_Classification_Code.rds")
+
+################################################################################
+# Q: Have total emissions from PM2.5 decreased in the United States from 1999  #
+# to 2008? Using the base plotting system, make a plot showing the total PM2.5 #
+# emission from all sources for each of the years 1999, 2002, 2005, and 2008.  #
+################################################################################
+
+## Save as png
+# png("plot1.png", width = 460, height = 480)
+
+totalEmissions <- aggregate(Emissions ~ year, NEI, sum) # aggregate emissions by year
+colours <- c("red", "yellow", "blue", "green") # set colours for barplot
+with(totalEmissions, 
+     barplot(height=Emissions/1000, names.arg = year, col = colours, 
+             xlab = "Year", ylab = "Annual Emissions",
+             main = expression('Annual Emission PM'[2.5]*' from 1999 to 2008')))
+
+# dev.off()
+
+
+
+
